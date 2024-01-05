db37291f31c24cb45df0f027cf5bc8b2b6cfc0f6
OnlyFor <OnlyFor@users.noreply.github.com>
2024-01-05 10:44:26 +0800

    Update PromptEditorClass.ts

--- src/Compoents/PromptEditor/PromptEditorClass.ts
+++ src/Compoents/PromptEditor/PromptEditorClass.ts
@@ -6,7 +6,7 @@ export const LOCAL_TRANSLATE_SERVER = process.env.LOCAL_TRANSLATE_HOST
               ? process.env.LOCAL_TRANSLATE_HOST
               : "//" + process.env.LOCAL_TRANSLATE_HOST
       }/prompt-studio`
-    : "http://localhost:39011/prompt-studio"
+    : "0.0.0.0:443/prompt-studio"
 
 
 
